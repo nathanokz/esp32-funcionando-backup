78a2dc50a049edef58fbf413e3bfcad6948f0572
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-11-20 22:50:57 -0300

    Update client_udp.py

--- client_udp.py
+++ client_udp.py
@@ -1,30 +1,55 @@
 import socket #bilbioteca utilizada para comunicação entre computadores
+import threading #biblioteca utilizada para executar multiplas tarefas ao mesmo tempo
+import time
 
-host = '192.168.1.105' #ip do servidor
-porta = 5000 #porta do servidor 
+host = '192.168.1.105' #ip do servidor que o cliente vai se conectar
+porta = 5000 #porta do servidor que o cliente vai se conectar
 
-clientes = {} #cria um dicionario de clientes
+nome = "cliente_udp" #nome padrão do cliente
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #cria um socket udp
-server.bind((host, porta)) #conecta o socket udp no host e porta especificados
+metodoSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #cria um socket tcp
+metodoSocket.sendto(nome.encode(), (host, porta)) #conecta o socket tcp no host e porta especificados
 
-print('aguardando mensagem...') #printa enquanto espera uma mensagem
+def receber(): #serve para receber mensagens dos servidor
+    while True: #cria um loop infinito
+        try: #tenta realizar o bloco
+            data, ender = metodoSocket.recvfrom(1024) #recebe mensagens de ate 1024 bytes
+            print(data.decode()) #printa a mensagem recebida já decodificada byte -> string 
+        
+        except: #se não conseguir realizar o bloco cai aqui
+            break #finaliza o loop
+
+thread = threading.Thread(target=receber) #cria a thread que vai executar a função receber
+thread.daemon = True # define a thread como daemon que vai parar automaticamente quando o programa principal terminar
+thread.start() #inicia a thread
 
 while True: #inicia o loop
-    data, ender = server.recvfrom(20000) #recebe mensagens de ate 1024 bytes 
-    mensagem = data.decode().strip() #decodifica a mensagem recebida bytes -> string
+    mensagem = input('digite uma mensagem: ') #pede para digitar uma mensagem
 
-    if ender not in clientes: #verifica se o endereço do cliente ai não esta no dicionario
-        clientes[ender] = mensagem #adiciona o cliente ao dicionario
-        print(f"novo cliente: {mensagem} - {ender}") #printa que um cliente se conectou
-        for cliente_ender in clientes: #percorre todos os clientes do dicionario
-            if cliente_ender != ender: #evita mandar a mensagem de entrada para o cliente para o proprio cliente
-                server.sendto(f"servidor: {mensagem} entrou no chat.".encode(), cliente_ender) #envia uma mensagem de boas vindas
-        continue #espera pela proxima mensagem
+    if mensagem.startswith("/nick "): #verifica se a mensagem começa com /nick
+        nome = mensagem.split(" ", 1)[1] #atualiza o nome do cliente para o nick inserido
+        print("nome alterado para: ", nome) #mostra para qual nick o nome foi alterado
+        metodoSocket.sendto(nome.encode(), (host, porta)) #envia a mensagem codificada para oservidor
 
-    nome = clientes[ender] #pega o nome do cliente do dicionario
-    mensagem_completa = f"{nome}: {mensagem}" #formata a mensagem completa do cliente
-    print(mensagem_completa) #printa a mensagem completa
-    for cliente_ender in clientes: #percorre todos os clientes conectados
-        if cliente_ender != ender: #evita mandar a mensagem de entrada para o cliente para o proprio cliente
-            server.sendto(mensagem_completa.encode(), cliente_ender) #envia a mensagem para os clientes
+    elif mensagem.startswith('/bench'): #se a mensagem começa com /bench
+        try: #tenta executar o bloco
+            tamanho = int(mensagem.split(' ')[1]) #pega o valor de bench e converte para inteiro
+            print(f'enviando {tamanho} bytes...') #mostra quantos bytes esta enviando
+            dados = b'-' * tamanho #cria um bloco de dados com o tamanho inserido
+            inicio = time.time() #marca o inicio 
+            metodoSocket.sendto(dados, (host, porta)) #envia os dados
+            fim = time.time() #marca o fim
+            print(f'envio conculido levou: {fim-inicio} segundos') #mostra quanto tempo levou
+        except: #se nao conseguir executar o bloco cai aqui
+            print('erro ao executar bench') #exibe erro
+            continue #tenta novamente
+
+    elif mensagem == "/sair": #se a mensagem for igual a /sair
+        metodoSocket.sendto(f"{nome} saiu do servidor".encode(), (host, porta)) #envia a mensagem para o servidor dizendo que o cliente saiu
+        break #finaliza o loop saindo do servidor
+
+    else: #se não
+        metodoSocket.sendto(f"{nome}: {mensagem}".encode(), (host, porta)) #envia a mensagem para o servidor
+
+metodoSocket.close() #encerra o socket tcp
+print("fechando conexão") #sai do servidor
