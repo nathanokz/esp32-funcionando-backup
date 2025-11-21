a74e16e89d1fd8191646a1e0b9d3f6cdd54ac3ef
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-11-20 22:50:35 -0300

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -1,5 +1,6 @@
 import socket #bilbioteca utilizada para comunicação entre computadores
 import threading #biblioteca utilizada para executar multiplas tarefas ao mesmo tempo
+import time
 
 host = '192.168.1.105' #ip do servidor que o cliente vai se conectar
 porta = 5000 #porta do servidor que o cliente vai se conectar
@@ -31,6 +32,19 @@ while True: #inicia o loop
     if mensagem.startswith("/nick "): #verifica se a mensagem começa com /nick
         nome = mensagem.split(" ", 1)[1] #atualiza o nome do cliente para o nick inserido
         print("nome alterado para: ", nome) #mostra para qual nick o nome foi alterado
+
+    elif mensagem.startswith('/bench'): #se a mensagem começa com /bench
+        try: #tenta executar o bloco
+            tamanho = int(mensagem.split(' ')[1]) #pega o valor de bench e converte para inteiro
+            print(f'enviando {tamanho} bytes...') #mostra quantos bytes esta enviando
+            dados = b'-' * tamanho #cria um bloco de dados com o tamanho inserido
+            inicio = time.time() #marca o inicio
+            metodoSocket.sendall(dados) #envia os daods
+            fim = time.time() #marca o fim
+            print(f'envio conculido levou: {fim-inicio} segundos') #mostra quanto tempo levou
+        except: #se nao conseguir executar o bloco cai aqui
+            print('erro ao executar bench') #exibe erro
+            continue #tenta novamente
 
     elif mensagem == "/sair": #se a mensagem for igual a /sair
         metodoSocket.send(f"{nome} saiu do servidor".encode()) #envia a mensagem para o servidor dizendo que o cliente saiu
